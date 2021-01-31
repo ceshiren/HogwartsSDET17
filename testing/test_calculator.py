adb210f3d95f932a8fdb95808e156794e76004cb
xj418974188 <xujuan418974188@163.com>
2021-01-31 14:35:02 +0800

    pytest 第一节课作业，异常情况处理，用例设计

--- testing/test_calculator.py
+++ testing/test_calculator.py
@@ -11,16 +11,20 @@ print(sys.path)
 from pythoncode.Calculator import Calculator
 
 
-def get_datas():
+def get_datas(name, type='int'):
     with open("./datas/calc.yml") as f:
-        datas = yaml.safe_load(f)
-    return (datas['add']['datas'], datas['add']['ids'])
+        all_datas = yaml.safe_load(f)
+    datas = all_datas[name][type]['datas']
+    ids = all_datas[name][type]['ids']
+    return (datas, ids)
 
 
 # yaml json excel csv xml
 # 测试类
 class TestCalc:
-    datas: list = get_datas()
+    # datas: list = get_datas()
+    add_int_data = get_datas('add', 'int')
+    div_int_data = get_datas('div', 'int_error')
 
     # 前置条件
     def setup_class(self):
@@ -31,18 +35,21 @@ class TestCalc:
     def teardown_class(self):
         print("结束计算")
 
-    @pytest.mark.parametrize("a, b, result", datas[0], ids=datas[1])
+    @pytest.mark.parametrize("a, b, result", add_int_data[0], ids=add_int_data[1])
     def test_add(self, a, b, result):
         print(f"a={a} , b ={b} ,result={result}")
         assert result == self.calc.add(a, b)
 
-    def test_add1(self):
-        datas = [[1, 1, 2], [100, 400, 300], [1, 0, 1]]
-        for data in datas:
-            print(data)
-            assert data[2] == self.calc.add(data[0], data[1])
+    @pytest.mark.parametrize("a,b,result", [
+        [0.1, 0.1, 0.2],
+        [0.1, 0.2, 0.3]
+    ])
+    def test_add_float(self, a, b, result):
+        assert result == round(self.calc.add(a, b), 2)
 
     # TODO: 完善相加功能
     # TODO: 相除功能
-    def test_div(self):
-        pass
+    @pytest.mark.parametrize("a, b, result", div_int_data[0], ids=div_int_data[1])
+    def test_div_0(self, a, b, result):
+        with pytest.raises(ZeroDivisionError, TypeError):
+            result = a / b
